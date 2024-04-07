465803f3ecf596f0fee2a4dc279e4bd985d5ee97
yunJ00 <heather2000@naver.com>
2024-04-07 18:46:47 +0900

    Fix: fuction 반복문으로 설정

--- add_dog_face.py
+++ add_dog_face.py
@@ -14,30 +14,30 @@ face_landmark_predictor_path = 'landmarkDetector.dat'
 detector = dlib.cnn_face_detection_model_v1(face_landmark_detector_path)
 predictor = dlib.shape_predictor(face_landmark_predictor_path)
 
-first_face = ("images/coco1.jpg", "coco")
-second_face = ("images/song5.jpg", "song")
+known_face = [("images/coco1.jpg", "coco"), ("images/song5.jpg", "song")]
 
 class Add_dog_face:
     def __init__(self):
         self.known_face_encodings = []   
         self.known_face_names = []
     
-    def add_known_face(self, face_image_path, name):
+    def add_known_face(self, known_face):
         Finding = Find_dog_face()
         target_width = 200
-        image = Finding.resize_image(face_image_path, target_width)
-        dets_locations = face_locations(image, 1)
-        face_encodings = face_recognition.face_encodings(image, dets_locations)
+        for face_image_path, name in known_face:   
+            image = Finding.resize_image(face_image_path, target_width)
+            dets_locations = face_locations(image, 1)
+            face_encodings = face_recognition.face_encodings(image, dets_locations)
 
-        for face_encoding, location in zip(face_encodings, dets_locations):
-            detected_face_image = draw_label(image, location, name)
-            self.known_face_encodings.append(face_encoding)
-            self.known_face_names.append(name)
+            for face_encoding, location in zip(face_encodings, dets_locations):
+                detected_face_image = draw_label(image, location, name)
+                self.known_face_encodings.append(face_encoding)
+                self.known_face_names.append(name)
+                
+            Finding.plt_imshow(["Input Image", "Detected Face"], [image, detected_face_image], result_name='known_face.jpg')
         
         np.save('known_faces.npy', self.known_face_encodings)
         np.save('known_names.npy', self.known_face_names)
-            
-        Finding.plt_imshow(["Input Image", "Detected Face"], [image, detected_face_image], result_name='known_face.jpg')
 
 def _trim_css_to_bounds(css, image_shape):
     return max(css[0], 0), min(css[1], image_shape[1]), min(css[2], image_shape[0]), max(css[3], 0)
@@ -62,8 +62,7 @@ def main():
     # finding = Find_dog_face(target_image)
     adding = Add_dog_face()
     # finding.finding(debug=True)
-    adding.add_known_face(first_face[0], first_face[1])
-    adding.add_known_face(second_face[0], second_face[1])
+    adding.add_known_face(known_face)
     
 if __name__ == '__main__':
     main()
