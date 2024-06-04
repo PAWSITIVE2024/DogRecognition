5ab4fe335ed66c87a8868ea4e40dcaa17314617e
yunJ00 <heather2000@naver.com>
2024-06-04 11:17:23 +0900

    Test: 테스트 종료 및 저장 위치 변경

--- add_dog_face.py
+++ add_dog_face.py
@@ -65,10 +65,8 @@ class Add_dog_face:
                     self.known_face_encodings.append(face_encoding)
                     self.known_face_names.append(name)
             self.face_specifics.append(face_specific)
-        print("Saving Staraaaaaaaaaaaaaaat")
-        np.save('library/known_faces.npy', self.known_face_encodings)
-        np.save('library/known_names.npy', self.known_face_names)
-        print("Doooooooooooooooooone")
+        np.save('numpy/known_faces.npy', self.known_face_encodings)
+        np.save('numpy/known_names.npy', self.known_face_names)
 
 def _trim_css_to_bounds(css, image_shape):
     return max(css[0], 0), min(css[1], image_shape[1]), min(css[2], image_shape[0]), max(css[3], 0)
