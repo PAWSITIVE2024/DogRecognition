2e91fa911ef94892440ca6f74f521a141643d508
yunJ00 <heather2000@naver.com>
2024-06-10 22:41:53 +0900

    Feat: 블루투스 연결 기능 추가

--- bluetooth.py
+++ bluetooth.py
@@ -1,55 +1,45 @@
 import bluetooth
 
-class BluetoothServer:
-    def __init__(self, uuid, port=1):
-        self.uuid = uuid
+class Bluetooth:
+    def __init__(self, port=1):
+        self.server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
         self.port = port
-        self.server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        self.client_sock = None
-        self.client_info = None
+        self.client_socket = None
+        self.address = None
 
     def start_server(self):
-        self.server_sock.bind(("", self.port))
-        self.server_sock.listen(1)
-        bluetooth.advertise_service(self.server_sock, "BluetoothServer",
-                                    service_id=self.uuid,
-                                    service_classes=[self.uuid, bluetooth.SERIAL_PORT_CLASS],
-                                    profiles=[bluetooth.SERIAL_PORT_PROFILE])
+        self.server_socket.bind(("", self.port))
+        self.server_socket.listen(1)
         print("Waiting for connection...")
-        self.client_sock, self.client_info = self.server_sock.accept()
-        print(f"Accepted connection from {self.client_info}")
 
-    def stop_server(self):
-        if self.client_sock:
-            self.client_sock.close()
-        self.server_sock.close()
-        print("Server stopped.")
+    def accept_connection(self):
+        self.client_socket, self.address = self.server_socket.accept()
+        print(f"Accepted connection from {self.address}")
 
-    def receive_user_id(self):
+    def receive_data(self):
         try:
-            data = self.client_sock.recv(1024).decode('utf-8')
-            print(f"Received user_id: {data}")
-            return data
-        except OSError as e:
-            print(f"Error receiving data: {e}")
-            return None
+            data = self.client_socket.recv(1024)
+            if data:
+                return data.decode('utf-8')
+        except OSError:
+            pass
+        return None
 
-    def send_data(self, weight):
-        try:
-            self.client_sock.send(str(weight).encode('utf-8'))
-            print(f"Sent weight: {weight}")
-        except OSError as e:
-            print(f"Error sending data: {e}")
+    def close_connection(self):
+        if self.client_socket:
+            self.client_socket.close()
+        self.server_socket.close()
+        print("Disconnected")
+
+    def main():
+        bluetooth_server = Bluetooth()
+        bluetooth_server.start_server()
+        bluetooth_server.accept_connection()
+        user_id = bluetooth_server.receive_data()
+        bluetooth_server.close_connection()
+        return user_id
 
 if __name__ == "__main__":
-    server_uuid = "00001101-0000-1000-8000-00805F9B34FB"
-    server = BluetoothServer(uuid=server_uuid)
-    server.start_server()
-
-    user_id = server.receive_user_id()
-    if user_id:
-        # 여기서 그릇 무게 받아오는 거 필요함
-        weight = 70  # 예씨
-        server.send_data(weight)
-
-    server.stop_server()
+    bluetooth = Bluetooth()
+    bluetooth.main()
+    
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import numpy as np
 from link_with_firebase import Link_firebase
 from add_dog_face import Add_dog_face
 from facial_recognition_for_dogs import Dog_facial_recognition
-from bluetooth import BluetoothServer
+from bluetooth import Bluetooth
 from Sending import Sending
 
 class Run:
@@ -14,15 +14,11 @@ class Run:
         self.user_id = None
     
     def run(self):
-        if self.step == 0:
-            # 여기서 안드로이드랑 블루투스로 연결하는 코드 계속 돌림..
-            # 연결되면 그거 user_id로 선언하고 loop 종료 후 step1으로 넘어감.
-            # get userID from android
-            # user_id = '9OccDgcNSbcOl2limdvnFtcAb4E2'
-            server = BluetoothServer(self.data_received)
-            while self.user_id is None:
-                time.sleep(1)
-            self.step += 1
+        if self.step == 0: # 블루투스 연결하기
+            bluetooth = Bluetooth()
+            self.user_id = bluetooth.main()
+            if self.user_id is not None:
+                self.step += 1
         elif self.step == 1: # 이미지 저장하기
             link_firebase = Link_firebase(user_id)
             link_firebase.save_image()
@@ -49,10 +45,6 @@ class Run:
             sending.sending(detected_name)
             if sending.process_done == True:
                 self.step == 3
-
-    def data_received(self, data):
-        # 블루투스를 통해 데이터 수신
-        self.user_id = data
         
 def main():
     run = Run()
