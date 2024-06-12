f5b24f9e9f0ed8a97c93e0ac8054aae923eb0da3
yunJ00 <heather2000@naver.com>
2024-06-13 01:56:16 +0900

    Fix: 아두이노코드 합치기

--- starting.py
+++ starting.py
@@ -5,11 +5,13 @@ import subprocess
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 # 가상환경 경로 설정
-script_path = '/home/pi/final_IoT.py'
+venv_path = '/home/ahnselim/myenv/bin/activate'
+script_path = '/home/ahnselim/DogRecognition/final_IoT.py'
 
 while True:
     if ser.in_waiting > 0:
         line = ser.readline().decode('utf-8').rstrip()
         if line == "ButtonPressed":
-            print("Button pressed! Executing script in virtual environment...")
-            subprocess.run(['python3', script_path])
+            print("Button pressed! Activating virtual environment and executing script...")
+            subprocess.run(['bash', '-c', f'source {venv_path} && python {script_path}'])
+            break
--- starting/starting.ino
+++ starting/starting.ino
@@ -1,17 +1,45 @@
-const int buttonPin = 2;
-int buttonState = 0;
+#include <Wire.h>
+#include <Arduino.h>
+#include <HX711.h>
+
+const int loadCellPinDT = A0; // HX711의 DT핀과 연결된 아두이노 핀 번호
+const int loadCellPinSCK = A1; // HX711의 SCK핀과 연결된 아두이노 핀 번호
+const byte I2C_ADDRESS = 0x08; // I2C 주소
+const int buttonPin = 2; // 버튼 핀 번호
+
+float scaleDivide = 100000; // 무게 보정
+
+HX711 scale;
 
 void setup() {
-  pinMode(buttonPin, INPUT);
-  Serial.begin(9600);
+  Serial.begin(9600); // Baud Rate 설정
+  Wire.begin(I2C_ADDRESS); // I2C 주소 설정
+  Wire.onRequest(requestEvent); // I2C 요청 이벤트 설정
+
+  scale.begin(loadCellPinDT, loadCellPinSCK); // 연결
+  scale.set_scale(); // 기본 스케일 값 설정
+  scale.tare(); // 처음 시작시 0점 잡기
+
+  pinMode(buttonPin, INPUT); // 버튼 핀을 입력으로 설정
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
+  // while (digitalRead(buttonPin) == LOW) {
+  // }
+  delay(3000);
+  Serial.println("ButtonPressed");
+  break;
 
-  if (buttonState == HIGH) {
-    Serial.println("ButtonPressed");
-    delay(1000);
+  // 나머지 코드 실행
+  while (true) {
+    // 무게 측정 및 I2C로 데이터 전송
+    delay(1000); // 1초 delay (1초마다 반복)
   }
 }
 
+void requestEvent() {
+  float weight = scale.get_units() * 1000; // 무게를 gram 단위로 읽음
+  byte weightBytes[sizeof(float)];
+  memcpy(weightBytes, &weight, sizeof(weight));
+  Wire.write(weightBytes, sizeof(weightBytes)); // 무게 데이터를 I2C로 전송
+}
